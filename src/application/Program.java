3cbff441f234674933c417d306144205a6c33665
Rennan Nogueira <rmelo.nogueira123@gmail.com>
2022-09-14 13:31:58 -0300

    Project Done

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import entities.Product;
 
@@ -15,6 +16,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
+
 		List<Product> list = new ArrayList<>();
 		try (Scanner sc = new Scanner(System.in)) {
 			System.out.println("Enter file full path: ");
@@ -23,14 +25,23 @@ public class Program {
 
 				String line = br.readLine();
 				while (line != null) {
-					String [] fields = line.split(",");
-					list.add(new Product (fields[0],Double.parseDouble(fields[1])));
+					String[] fields = line.split(",");
+					list.add(new Product(fields[0], Double.parseDouble(fields[1])));
 					line = br.readLine();
 				}
+
+				double avg = list.stream().map(p -> p.getPrice()).reduce(0.0, (x, y) -> x + y) / list.size();
+
+				System.out.println("Average Price: " + String.format("%.2f", avg));
+				
+				list.removeIf(p -> p.getPrice()>avg);
+				list.sort(new MyComparator().reversed());
+				List<String> name = list.stream().map(p->p.getName()).collect(Collectors.toList());
+				name.forEach(System.out::println);
 			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
-		System.out.println(Arrays.toString(list.toArray()));
+		
 	}
 }
